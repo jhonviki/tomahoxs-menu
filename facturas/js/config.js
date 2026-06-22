4a3de92838df43b24dfde913008386f25a96b94d
jhonviki <jhomorsa@gmail.com>
2026-06-22 15:59:24 -0500

    deploy: facturas 2026-06-22

---
+++ facturas/js/config.js
@@ -0,0 +1,8 @@
+// js/config.js
+const CONFIG = {
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwjcOs15KtXkLW4-7fOl_6Tks8D_cRlvfM1PcOUp1v6VwjqBG9y4zyQMkgSYkue6HYj9Q/exec',
+  PEDIDOS_URL: 'https://script.google.com/macros/s/AKfycbzC0ejdZ_5NwkqY77s2e_2SQZtUvvBFRr2mf0yx3KT0m8UeH_4lgMVMlXBWuP-Jjd39/exec',
+  GEMINI_API_KEY: 'AIzaSyBBpEvh_Gk1kFFQd5n8vEjcPmk3VsjxCrk',
+  GEMINI_MODEL: 'gemini-2.5-flash',
+  MES_ACTUAL: new Date().toISOString().substring(0, 7) // 'YYYY-MM'
+};
