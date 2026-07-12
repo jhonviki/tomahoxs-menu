f28794975347f97fd555fc066f808eed92f662f7
jhonviki <jhomorsa@gmail.com>
2026-07-12 14:38:26 -0500

    deploy: facturas 2026-07-12

--- facturas/js/router.js
+++ facturas/js/router.js
@@ -1,6 +1,6 @@
 // js/router.js
 const ROUTER = {
-  screens: ['dashboard', 'scan', 'historial', 'recetas', 'costos', 'inventario'],
+  screens: ['dashboard', 'scan', 'historial', 'recetas', 'costos', 'inventario', 'libro'],
   current: null,
 
   init() {
